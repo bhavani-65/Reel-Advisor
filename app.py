e2aab402a67a227c6c25ba7227bcddd13a5064bd
Bhavani Vuppuloju <133529778+bhavani-65@users.noreply.github.com>
2024-06-25 00:52:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 
 # URL to the GitHub release asset
 # RELEASE_URL = 'https://github.com/bhavani-65/Reel-Advisor/releases/download/v1.0.0/similarity.tar.gz'
-tar_file_path = 'similarity.tar.gz'
+tar_file_path = 'similarity.tar.gz.part-aa'
 extract_folder = os.path.dirname(tar_file_path)
 
 # # Function to download the file from the release URL
